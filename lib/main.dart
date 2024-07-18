e2c59fee6238413558f92ab0f253904e5791c165
Yujin <tkfkd7038@naver.com>
2024-07-18 14:43:52 +0900

    Feat(main): 플러터 프레임워크가 앱을 실행할 준비가 됐는지 확인
    StatelessWidget에서 WebViewController를 프로퍼티로 직접 인스턴스화하기 위해 ensureInitizlied( ) 함수를 직접 실행함

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:blog_web_app/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
